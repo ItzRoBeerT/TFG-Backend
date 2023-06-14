b6d6588a8c8717a0a1ce2da9f8bb972e43d28052
Roberto <105317145+ItzRoBeerT@users.noreply.github.com>
2023-06-14 21:58:24 +0200

    Actualizacion titulo swagger

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ const swaggerOptions = {
     definition: {
         openapi: "3.0.0",
         info: {
-            title: "Social Media API",
+            title: "Pocket SM API",
             version: "1.0.0",
         },
         
